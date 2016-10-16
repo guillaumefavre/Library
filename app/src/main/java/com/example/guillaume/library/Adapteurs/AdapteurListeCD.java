c315f4afbf09bcc5b81d9868e0311cdb332183b7
gfavre <guillaumefavre56@gmail.com>
2016-10-16 14:38:40 +0200

    - Changement couleur items sélectionnés

--- app/src/main/java/com/example/guillaume/library/Adapteurs/AdapteurListeCD.java
+++ app/src/main/java/com/example/guillaume/library/Adapteurs/AdapteurListeCD.java
@@ -1,6 +1,8 @@
 package com.example.guillaume.library.Adapteurs;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +14,12 @@ import com.example.guillaume.library.Metier.CD;
 import com.example.guillaume.library.R;
 import com.example.guillaume.library.Utils.BitmapUtils;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by guillaume on 13/08/15.
@@ -31,11 +38,62 @@ public class AdapteurListeCD extends ArrayAdapter<CD> {
      */
     private ViewHolder holder;
 
+    /**
+     * Map représentant les éléments sélectionnés
+     */
+    private Map<Integer, CD> itemsSelectionnes = new HashMap<Integer, CD>();
 
+    /**
+     * Liste des CDs sélectionnés
+     */
+    private List<CD> listeCDsSelectionnes = new ArrayList<>();
+
+
+    /**
+     * Constructeur
+     * @param context
+     * @param listeCDs
+     */
     public AdapteurListeCD(final Context context, final List<CD> listeCDs) {
         super(context, R.layout.layout_liste_cd_item, listeCDs);
         inflater = LayoutInflater.from(context);
     }
+
+    /**
+     * Ajout d'un élément dans la map des items sélectionnés
+     * @param position
+     * @param value
+     */
+    public void addItemSelectionne(int position, CD value) {
+        itemsSelectionnes.put(position, value);
+        notifyDataSetChanged();
+    }
+
+
+    /**
+     * Suppression d'un item sélectionné
+     * @param position
+     */
+    public void removeItemSelectionne(int position) {
+        itemsSelectionnes.remove(position);
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Récupération des CDs sélectionnés
+     * @return
+     */
+    public Collection<CD> getCDsSelectionnes() {
+        return itemsSelectionnes.values();
+    }
+
+
+    public void clearSelection() {
+        itemsSelectionnes = new HashMap<Integer, CD>();
+        notifyDataSetChanged();
+    }
+
+
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -54,6 +112,10 @@ public class AdapteurListeCD extends ArrayAdapter<CD> {
             convertView.setTag(holder);
         }
 
+        // Couleur par défaut
+        convertView.setBackgroundColor(Color.parseColor("#EEEEEE"));
+
+
         // On récupère le holder à partir du tag
         holder = (ViewHolder) convertView.getTag();
 
@@ -62,12 +124,21 @@ public class AdapteurListeCD extends ArrayAdapter<CD> {
         holder.titreAlbum.setText((CharSequence) cd.getTitreAlbum());
 
         if(cd.getPochette() != null) {
-            holder.pochette.setImageBitmap(BitmapUtils.convertEncodedBase64StringToBitmap(cd.getPochette()));
+            // Récupération du bitmap d'origine à partir de la pochette
+            Bitmap bitmapOriginal = BitmapUtils.convertEncodedBase64StringToBitmap(cd.getPochette());
+
+            // Redéfinition de la taille du bitmap
+            Bitmap bitmapReduit = Bitmap.createScaledBitmap(bitmapOriginal, (int)(bitmapOriginal.getWidth()*0.6), (int)(bitmapOriginal.getHeight()*0.6), true);
+            holder.pochette.setImageBitmap(bitmapReduit);
         } else {
             // TODO gérer pochette absente
             holder.pochette.setImageBitmap(null);
         }
 
+        // Sélection d'un item, changement de la couleur de fond
+        if (itemsSelectionnes.get(position) != null) {
+            convertView.setBackgroundColor(Color.GRAY);
+        }
 
         return convertView;
     }
